ade0b37906bbe7bb025b9248b3fcbe7d48f95cd3
Alberto Pacheco Catalan <alberto.pacheco@kerabenet.local>
2021-12-02 15:25:22 +0100

    ready for heroku

--- src/index.js
+++ src/index.js
@@ -9,6 +9,7 @@ const path = require('path');
 app.set('port', 3000);
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
+app.use(express.static(path.join(__dirname, 'public'))); // All the files in this file will be public for the browser.
 
 // middlewares
 
@@ -17,7 +18,7 @@ app.use(require('./routes/')); // provide to the app the routes.
 // static files
 
 // The server is listening
-app.listen(app.get('port'), () => {
+app.listen(process.env.PORT, () => {
     console.log(`Server on port: ${app.get('port')}`);
 });
 
